6faad0a3959eeb42566be34c59ea534c19723afa
xenia11 <dasicksenija@gmail.com>
2023-03-27 16:44:53 +1100

    added font style

---
+++ index.js
@@ -0,0 +1,15 @@
+const midScreenIcons = [
+    { image: "./images/gmail.png", title: "Gmail" },
+    { image: "./images/googledrive.png", title: "Google Drive" },
+    { image: "./images/linkedin.png", title: "LinkedIn" },
+    { image: "./images/youtube.png", title: "YouTube" },
+    { image: "./images/slack.png", title: "Slack" },
+];
+
+const lowerScreenIcons = [
+    "./images/phone.png",
+    "./images/chrome.png",
+    "./images/sms1.png",
+    "./images/gallery.png",
+    "./images/menu.png",
+];
