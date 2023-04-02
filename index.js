6313e1cd66e3ee9bc49cd1d5935aeccfd5d3ca9a
xenia11 <dasicksenija@gmail.com>
2023-04-02 22:03:36 +1000

    added responsiveness

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// import { currentTime, dayOrNight } from "./javascript/time";
+
+// currentTime();
+// dayOrNight();
+
 const midScreenApps = [
     {
         image: "./images/gmail.png",
@@ -219,4 +224,33 @@ const closeAllModules = () => {
 const closeButton = document.getElementById("homeBtn");
 closeButton.addEventListener("click", closeAllModules);
 
-//make sure to find text contect for main icons
+const currentTime = () => {
+    function addZero(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+    const d = new Date();
+    let h = addZero(d.getHours());
+    let m = addZero(d.getMinutes());
+    let time = h + ":" + m;
+    document.getElementById("time").innerHTML = time;
+    setTimeout(currentTime, 100);
+    return h;
+};
+
+currentTime();
+
+const dayOrNight = (hours) => {
+    const currentHour = currentTime();
+    const dayNightImg = document.querySelector(".upper-screen__weather");
+
+    console.log(currentHour >= 20 || currentHour < 05);
+
+    currentHour >= 20 || currentHour < 05
+        ? (dayNightImg.src = "./images/nighttime.png")
+        : (dayNightImg.src = "./images/daytime.png");
+};
+
+dayOrNight();
